3fe5ad646f8924c6696316c2ba9211d536ac027e
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-09-15 16:19:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,12 @@
 def unsafe?(answer)
   #answer > 60 ? true : false
   #answer < 40 ? true : false
-  if answer < 40
+  if answer > 60
     return true
+  elsif answer < 40
+    return true
+  else 
+    return false
   end
 end
 
